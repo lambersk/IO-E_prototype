39eee6b16117cc9c72e96187ebeb9440f8ef0baa
lambersk <79655701+lambersk@users.noreply.github.com>
2021-12-18 10:31:47 -0500

    Update sketch.js

--- Prototype_ForestAmbience_2021_12_14_04_36_18/sketch.js
+++ Prototype_ForestAmbience_2021_12_14_04_36_18/sketch.js
@@ -1,212 +1,151 @@
-/* Code Prototype 
-Inspired by:
-https://experiments.withgoogle.com/ai/bird-sounds/view/
+// Code Final Design 
+// //Inspired by:
+// //https://experiments.withgoogle.com/ai/bird-sounds/view/
 
-Ambience Music Source:
-https://youtu.be/eLGGUDNf52g
+// //Ambience Music Source:
+// //https://youtu.be/eLGGUDNf52g
 
-Code references:
-- https://editor.p5js.org/son/sketches/rJcVOAI0Q
+// //Code references:
+// //- https://editor.p5js.org/son/sketches/rJcVOAI0Q //(Posenet by son)
+// //- https://editor.p5js.org/loulli/sketches/sPhbO5_Bf //(Body Tracking Color Changing by loulli )
 
-*/
+// //Animated Gif:
+// - //https://static.tumblr.com/cb1bef90995f8c70ff21e8db0f27f6cb/fmc6rxh/Mlgo18amh/tumblr_static_s8ehh1kso2ok0w8g4w088oco.gif
 
 
-let video;
-let poseNet;
-let poses = [];
-let skeletons = [];
-let song;
-let song2;
-let ebl;
-let ebr;
-let bl;
-let br;
-let bopl;
-let bopr;
-let ambience;
-let bg;
-let beak;
+var br;
+var gif_loadImg, gif_createImg;
+var gif_loadImg2, gif_createImg2;
+var song;
 
-var _imgSize = 700;
-var _img;
-var _ffCount = 100;
-var _ff = [];
+let snow = [];
+let gravity;
+let flow = 0;
+
 
 function preload() {
-  createCanvas(displayWidth, displayHeight);
-    background(128);
-    noStroke();
- 
-    imgSet();
- 
-    for (var i=0; i<_ffCount; i++) {
-        _ff[i] = new FfObj();
-    }
-  //Ambience music
-
-  ambience = loadSound("Ambience.mp3");
- // parotia = loadSound("parotia.mp3");
- // nightingale = loadSound("nightingale.mp3");
- // raven = loadSound("raven.mp3");
+ // bg = loadImage('test.JPEG');
+  gif_createImg = createImg("deerwalk.gif");
+  gif_createImg2 = createImg("sparkles.gif");
+  song = loadSound("Ambience.mp3");
 
 }
-
 function setup() {
-  frameRate(30);
-  bg = loadImage('rainforest.jpg');
+  bg = loadImage('test.JPEG');
+  createCanvas(windowWidth, windowHeight);
+  song.play();
   
-  createCanvas(650, 742);
+  //gif_createImg.resize(20,20);
+  gravity = createVector(0, 0.3);
 
-  riflebird.loop();
-  parotia.loop();
-  nightingale.loop();
-  raven.loop();
-
-  video = createCapture(VIDEO);
-  video.size(width, height);
-
-  poseNet = ml5.poseNet(video, modelReady);
-  poseNet.on('pose', function(results) {
-    poses = results;
-  });
-  //video.hide();
-}
-
-function modelReady() {
-  select('#status').html('Model Loaded');
+  for (let i = 0; i < 400; i++) {
+    let x = random(width);
+    let y = random(height);
+    snow.push(new Snowflake(x, y));
+  }
+  
 }
 
 function draw() {
-  background(bg);
-  
-      for (var i=0; i<_ffCount; i++) {
-        if (!mouseIsPressed) { _ff[i].drawMe();}
-        else {_ff[i].drawMe2();}
-        _ff[i].updateMe();
-    }
-  
+background(42, 73, 48);
 
+  // updates animation frames by using an html
+  // img element, positioning it over top of
+  // the canvas.
+  gif_createImg.position(400, 200);
+  gif_createImg2.position(5, 30);
 
-  leavesFall();
-}
-
-//the glowing circles code 
-
-function imgSet() {
-    _img = createImage(_imgSize, _imgSize);
-    _img.loadPixels();
-    for (var i = 0; i < _img.width; i++) {
-        for (var j = 0; j < _img.height; j++) {
-            var pixAlpha = 255/(dist(_img.width/2, _img.height/2, i, j)-1)*1.47;
-            if (pixAlpha < 0.94) { pixAlpha=0; }
-            _img.set(i, j, color(255, 255, 250, pixAlpha));
-        }
-    }
-    _img.updatePixels();
-}
- 
-function FfObj() {
-    this.pX = random(0-_img.width/2, width-_img.width/2);
-    this.pY = random(0-_img.height/2, height-_img.height/2);
-    this.noiseX = random()*1000;
-    this.noiseY = random()*1000;
-    this.noiseScale = random(0.001, 0.02);
-}
- 
-FfObj.prototype.updateMe = function() {
-    this.pX += noise(this.noiseX)*4-1.86;
-    this.pY += noise(this.noiseY)*4-1.86;
-    if (this.pX < 0-_img.width/2) { this.pX = 0-_img.width/2;}
-    if (this.pX > width-_img.width/2) { this.pX = width-_img.width/2;}
-    if (this.pY < 0-_img.height/2) { this.pY = 0-_img.height/2;}
-    if (this.pY > height-_img.height/2) { this.pY = height-_img.height/2;}
-    this.noiseX += this.noiseScale;
-    this.noiseY += this.noiseScale;
-}
- 
-FfObj.prototype.drawMe = function() {
-    image(_img, this.pX, this.pY);
-    fill(255, 255, 250, 255);
-    ellipse(this.pX+_img.width/2, this.pY+_img.height/2, 2);   
-}
- 
-FfObj.prototype.drawMe2 = function() {
-    fill(255, 255, 250, 255);
-    ellipse(this.pX+_img.width/2, this.pY+_img.height/2, 6);
-}
-
-//the falling leaves code
-
-function leavesFall() {
-  for (let i = 0; i < poses.length; i++) {
-    for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
-
-      //Left Wrist
-      let leftWrist = poses[i].pose.keypoints[9];
-      if (leftWrist.score > 0.2) {
-        push();
-        fill(0, 0, 255);
-        noStroke();
-        bopl.size(50, 50);
-        bopl.position(leftWrist.position.x, leftWrist.position.y, 10, 10, 10, 10);
-        pop();
-
-        riflebird.rate(map(constrain(leftWrist.position.y, 0, height), height, 0, 0.25, 2));
-      }
-
-      //Right Wrist
-      let rightWrist = poses[i].pose.keypoints[10];
-      if (rightWrist.score > 0.2) {
-        push();
-        fill(0, 0, 255);
-        noStroke();
-        bopr.size(50, 50);
-        bopr.position(rightWrist.position.x, rightWrist.position.y, 10, 10, 10, 10);
-        pop();
-
-        parotia.rate(map(constrain(rightWrist.position.y, 0, height), height, 0, 0.25, 2));
-
-      }
-
-
-      //Left Shoulder
-      let leftShoulder = poses[i].pose.keypoints[5];
-      if (leftShoulder.score > 0.2) {
-        push();
-        fill(0, 0, 255);
-        noStroke();
-        bl.size(50, 50);
-        bl.position(leftShoulder.position.x, leftShoulder.position.y, 10, 10, 10, 10);
-        pop();
-        
-                nightingale.rate(map(constrain(leftShoulder.position.y, 0, height), height, 0, 0.25, 2));
-        
-      }
-
-      //Right Shoulder
-      let rightShoulder = poses[i].pose.keypoints[6];
-      if (rightShoulder.score > 0.2) {
-        push();
-        fill(0, 0, 255);
-        noStroke();
-        br.size(50, 50);
-        br.position(rightShoulder.position.x, rightShoulder.position.y, 10, 10, 10, 10);
-        pop();
-        
-                raven.rate(map(constrain(rightShoulder.position.y, 0, height), height, 0, 0.25, 2));
-      }
-
-      //L/R shoulder line
-      push();
-      stroke(255,255,255,5);
-      strokeWeight(5);
-      line(leftShoulder.position.x, leftShoulder.position.y, rightShoulder.position.x, rightShoulder.position.y);
-      pop();
-
-
-   
-
-      }
-    }
+   for (flake of snow) {
+    flake.applyForce(gravity);
+    flake.update();
+    flake.render();
   }
+  
+//Reference to how to move the gif from https://editor.p5js.org/chjno/sketches/H1ZtuJ0Yb
+//     push();
+//     translate(flow, 0);
+//     flow++;
+//     image(gif_createImg, 20, 100);
+//      if(flow + 80 > width){
+//         flow = -145;
+//      }
+ 
+// pop();
+ 
+
+}
+function getRandomSize() {
+
+  let r = pow(random(0, 1), 3);
+  return constrain(r * 32, 2, 32);
+
+}
+
+class Snowflake {
+
+  constructor(sx, sy) {
+    let x = sx;
+    let y = sy;
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector();
+    this.angle = random(TWO_PI);
+    this.dir = (random(1) > 0.5) ? 1 : -1;
+    this.xOff = 0;
+    this.r = getRandomSize();
+  }
+
+  applyForce(force) {
+    // Parallax Effect hack
+    let f = force.copy();
+    f.mult(this.r);
+
+    this.acc.add(f);
+  }
+
+  randomize() {
+    let x = random(width);
+    let y = random(-100, -10);
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector();
+    this.r = getRandomSize();
+  }
+
+  update() {
+
+    this.xOff = sin(this.angle * 2) * 2 * this.r;
+
+    this.vel.add(this.acc);
+    this.vel.limit(this.r * 0.2);
+
+    if (this.vel.mag() < 1) {
+      this.vel.normalize();
+    }
+
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+
+    if (this.pos.y > height + this.r) {
+      this.randomize();
+    }
+
+    // Wrapping Left and Right
+    if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    }
+
+    this.angle += this.dir * this.vel.mag() / 200;
+  }
+
+  render() {
+    push();
+    translate(this.pos.x + this.xOff, this.pos.y);
+		ellipse(this.pos.x, this.pos.y, this.r, this.r)
+    pop();
+  }
+
 }
